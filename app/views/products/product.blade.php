8f8817c6222eb160ae860e4f949ececd758d2372
Yash pal <code.yash@gmail.com>
2014-01-04 18:29:05 +0530

    product page

--- app/views/products/product.blade.php
+++ app/views/products/product.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
 
-    <div class='desktop-8 '>
+    <div class='desktop-8 product-single'>
 
         @include('products.single',['product' => $product] )
 
     </div>
 
-    <div class='desktop-4 bg text-center'>
+    <div class='desktop-4 product-sidebar'>
         <h3>Deal of the day</h3>
         <img data-src='holder.js/350x300' src='{{$product->image}}' />
         <h3>Related Items</h3>
