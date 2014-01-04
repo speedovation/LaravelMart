9f48562dee8751fc6ebc0624988ee7e45d0ab701
Yash pal <code.yash@gmail.com>
2014-01-04 14:11:29 +0530

    product page

--- app/views/products/single.blade.php
+++ app/views/products/single.blade.php
@@ -1,11 +1,15 @@
 <table class="table">
     <tr>
-        <td>Product Name : {{$product->name}} #Product Code :{{$product->code}}
-
+        <td>
+            <h2 >Product Name : {{$product->name}} <button class='button success large float-right'> <i class='i-rupee'></i> {{round($product->price,3)}} ADD TO CART</button>
+            </h2>
         </td>
     </tr>
     <tr>
         <td> <img data-src='holder.js/400x300' src='{{$product->image}}' /></td>
+    </tr>
+    <tr>
+        <td> <span class='float-left'>Category : {{$product->category->name}}  #Product Code :{{$product->code}}</span> </td>
     </tr>
     <tr>
         <td>Price: {{$product->long_desc}}</td>
@@ -14,7 +18,7 @@
         <td>More options</td>
     </tr>
     <tr>
-        <td>Category : {{$product->category->name}} Stock: {{$product->stock}}</td>
+        <td>Stock: {{$product->stock}}</td>
     </tr>
     <tr>
         <td>Price: {{$product->price}}</td>
