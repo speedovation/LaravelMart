dd0dfb95a0b28ffc211a356151f6e9851790b1c6
Yash LINUX <code.yash@gmail.com>
2015-06-05 11:58:22 +0530

    Config and routes modified

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,86 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+//App::bind("Formativ\Billing\GatewayInterface", "Formativ\Billing\StripeGateway");
+//App::bind("Formativ\Billing\DocumentInterface", "Formativ\Billing\PDFDocument");
+//App::bind("Formativ\Billing\MessengerInterface", "Formativ\Billing\EmailMessenger");
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
+Route::any("/", [
+    "as" => "index/index",
+    "uses" => "IndexController@indexAction"
 ]);
+
+Route::any("category/index", [
+    "as" => "category/index",
+    "uses" => "CategoryController@indexAction"
+]);
+
+//Route::any("product/index", [
+//    "as" => "product/index",
+//    "uses" => "ProductController@indexAction"
+//]);
+//
+//Route::any("product/product/", [
+//    "as" => "product/index",
+//    "uses" => "ProductController@indexAction"
+//]);
+
+Route::any("account/authenticate", [
+    "as" => "account/authenticate",
+    "uses" => "AccountController@authenticateAction"
+]);
+
+Route::any("order/index", [
+    "as" => "order/index",
+    "uses" => "OrderController@indexAction"
+]);
+
+Route::any("order/add", [
+    "as" => "order/add",
+    "uses" => "OrderController@addAction"
+]);
+
+Route::any("order/delete", [
+    "as" => "order/delete",
+    "uses" => "OrderController@deleteAction"
+]);
+
+Route::get('/login', 'UsersController@getLogin');
+
+Route::controller('users', 'UsersController');
+Route::controller('products', 'ProductsController');
+Route::controller('cart', 'CartController');
+Route::controller('about', 'AboutController');
+
+Route::resource('wishlist', 'WishlistController');
+
+//Route::group(["before" => "guest"], function()
+//{
+//    Route::any("/", [
+//        "as"   => "user/login",
+//        "uses" => "UserController@loginAction"
+//    ]);
+//
+//    Route::any("/request", [
+//        "as"   => "user/request",
+//        "uses" => "UserController@requestAction"
+//    ]);
+//
+//    Route::any("/reset", [
+//        "as"   => "user/reset",
+//        "uses" => "UserController@resetAction"
+//    ]);
+//});
+//
+//Route::group(["before" => "auth"], function()
+//{
+//    Route::any("/profile", [
+//        "as"   => "user/profile",
+//        "uses" => "UserController@profileAction"
+//    ]);
+//
+//    Route::any("/logout", [
+//        "as"   => "user/logout",
+//        "uses" => "UserController@logoutAction"
+//    ]);
+//});
+
