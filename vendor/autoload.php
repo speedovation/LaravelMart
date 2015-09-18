25dcc03972d03e70b1dfc3c2ef01ba94eb0a39b8
Yash <yash@speedovation.com>
2015-09-18 15:58:23 +0530

    Vendor updated

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitcca97bd2843732b61df7ae5e42cfff9f::getLoader();
+return ComposerAutoloaderInit183611f92afe0900fae4a89e5d3ce572::getLoader();
