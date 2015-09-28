5e61a2a21972c1f27d9f15d2c74ef5d5f78d4692
Yash <yash@speedovation.com>
2015-09-28 13:47:28 +0530

    Look updated

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('kblayouts.main')
 
 @section('title', 'Homepage Title')
 
@@ -7,37 +7,37 @@
 @section('content')
 
 
+<section class="section">
 
-
-<div class="row">
-    
-    
-    <h1>
-        Latest KB Entries
-    </h1>
-    
-    
-    @foreach ($pages as $page)
-    <div class=" col-12">
-        <div class="panel panel-box panel-box-default blog-box">
-            <h2 class="kilo"> <a href="/kb/{{$page->url}}"> {{$page->title}} </a> </h2>
-            <span class="text-small blog-info"> Author: {{$page->author}} | Date : {{$page->created_at->format('F d, Y')}} </span>
-            <div>
-                {!! limit_words($page->body,600) !!}
-                <a href="/kb/{{$page->url}}" class="button button-royal button-rounded read-more">Read More</a>
+    <div class="row">
+        
+        <h1>
+            Latest KB Entries
+        </h1>
+        
+        
+        @foreach ($pages as $page)
+        <div class=" col-12">
+            <div class="panel panel-box panel-box-default blog-box">
+                <h2 class="kilo"> <a href="/kb/{{$page->url}}"> {{$page->title}} </a> </h2>
+                <span class="text-small blog-info"> Author: {{$page->author}} | Date : {{$page->created_at->format('F d, Y')}} </span>
+                <div>
+                    {!! limit_words($page->body,600) !!}
+                    <a href="/kb/{{$page->url}}" class="button button-royal read-more">Read More</a>
+                </div>
             </div>
         </div>
+        @endforeach
+        
+        
+        
     </div>
-    @endforeach
     
-    
-    
-</div>
+    <div class="row">
+        {!! $pages->render() !!}
+    </div>
 
-<div class="row">
-    {!! $pages->render() !!}
-</div>
-
+</section>
 
 
 @endsection
--- resources/views/blog/page.blade.php
+++ resources/views/blog/page.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('kblayouts.main')
 
 @section('title', $page->title)
 
@@ -9,32 +9,23 @@
 
 
 
+<section class="section">
 
-
-<div class="row wrap">
-    <hr/>
-
-
+    <div class="row wrap">
+        
+        
+         <div class="panel  blog-box single-box col-12">   
+                <h2 class="kilo">  <a href="/kb/{{$page->url}}">{{$page->title}} <i class="material-icons md-32">speaker_notes</i>  </a> </h2>
+                <span class="text-small blog-info"> Author: {{$page->author}} | Date : {{$page->created_at->format('F d, Y')}} </span>
+                <div>
+                    {!! $page->body !!} 
+                    <a href="/kb" class="button button-royal read-more">Home</a>
+                </div>
+             </div> 
         
     
+    </div>
     
-     <div class="panel panel-box  blog-box col-12">   
-            <h2 class="kilo">  <a href="/kb/{{$page->url}}"> <i class="material-icons md-32">info</i> {{$page->title}} </a> </h2>
-            <span class="text-small blog-info"> Author: {{$page->author}} | Date : {{$page->created_at->format('F d, Y')}} </span>
-            <div>
-                {!! $page->body !!} 
-                <a href="/kb" class="button info read-more">Back to Index</a>
-            </div>
-         </div> 
-    
-
-
-    
-
-
-
-
-
-</div>
+</section>    
 
 @endsection
